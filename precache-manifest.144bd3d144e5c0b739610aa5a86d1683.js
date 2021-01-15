9eca99e16364aa96187ca0d52b7d1fce7b931466
trifiasco <trifiasco@gmail.com>
2021-01-15 23:52:44 +0600

    Updates

---
+++ precache-manifest.144bd3d144e5c0b739610aa5a86d1683.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f70f0bce239104a21393493fdb4240cf",
+    "url": "/EBI-ot-front-end-test/index.html"
+  },
+  {
+    "revision": "2a3e59d23b062ebd59c1",
+    "url": "/EBI-ot-front-end-test/static/css/main.f0936c25.chunk.css"
+  },
+  {
+    "revision": "e4c01593a3a74142bd0d",
+    "url": "/EBI-ot-front-end-test/static/js/2.6679fe4b.chunk.js"
+  },
+  {
+    "revision": "6bdf6becedb14e4609964580f4c3faf6",
+    "url": "/EBI-ot-front-end-test/static/js/2.6679fe4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a3e59d23b062ebd59c1",
+    "url": "/EBI-ot-front-end-test/static/js/main.06817d48.chunk.js"
+  },
+  {
+    "revision": "7be6860c9b06131a619f",
+    "url": "/EBI-ot-front-end-test/static/js/runtime-main.40326b86.js"
+  }
+]);
